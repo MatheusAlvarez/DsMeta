82fb58d46e35bb2e1d93e4526e870afc161558f9
Matheus Maia Alvarez <mthalvarez0000@hotmail.com>
2022-07-13 18:20:25 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NotificationButton from "./components/NotificationsButton";
+
 function App() {
   return(
+    <>
     <h1>Olá mundo!</h1>
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    </>
   );
 }
 
