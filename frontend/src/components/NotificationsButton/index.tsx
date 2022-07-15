327e0e13f0aaf69c8841ff2373dc54171a406a03
Matheus Maia Alvarez <mthalvarez0000@hotmail.com>
2022-07-15 11:24:40 -0300

    Toast

--- frontend/src/components/NotificationsButton/index.tsx
+++ frontend/src/components/NotificationsButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import icon from "../../assets/img/notification-icon.svg";
 import { BASE_URL } from "../../utils/request";
 import "./styles.css";
@@ -11,7 +12,7 @@ type Props ={
 function handleClick(id: number){
     axios(`${BASE_URL}/sales/${id}/notification`)
     .then((response) => {
-        console.log("SECESSO")
+        toast.info("SMS Enviado com sucesso!!")
     })
     .catch((err) => {
         console.log(err)
