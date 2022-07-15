028e7b2ab7b34ccf85695190331bc7582c2c701b
Matheus Maia Alvarez <mthalvarez0000@hotmail.com>
2022-07-15 13:05:17 -0300

    Erro SMS

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,5 +1,9 @@
 package com.devsuperior.dsmeta.services;
 
+import java.text.DecimalFormat;
+
+import java.text.DecimalFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -34,8 +38,8 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date 
-				+ " com um total de R$" + String.format("%.2f", sale.getAmount());
+		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
+			    + " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
 		
 		Twilio.init(twilioSid, twilioKey);
 
